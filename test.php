ec9f0056e35502010ad715cb2e91b4af3cba4cb8
linjianghe <303522560@qq.com>
2019-04-18 17:56:56 +0800

    测试文件

---
+++ test.php
@@ -0,0 +1,37 @@
+<?php
+$arr = [1,3,2,5,4,1,2];
+var_dump(select_sort($arr));
+function bubble_sort($arr){
+	$count = count($arr);
+	if($count<=1){
+		return $arr;
+	}
+	for($i=1;$i<$count;$i++){
+		for($j=0;$j<$count-$i;$j++){ 
+			if($arr[$j]<$arr[$j+1]){ 
+				$tmp = $arr[$j+1];
+				$arr[$j+1] = $arr[$j];
+				$arr[$j] = $tmp;
+			}
+		}
+	}
+	return $arr;
+}
+function select_sort($arr){
+	$count = count($arr);
+	if($count<=1){
+		return $arr;
+	}
+	$base = $arr[0];
+	$left = $right = [];
+	for($i=1;$i<$count;$i++){
+		if($arr[$i]<$base){
+			$left[] = $arr[$i];
+		} else {
+			$right[] = $arr[$i];		
+		}
+	}
+	$left = select_sort($left);
+	$right= select_sort($right);
+	return array_merge($left,[$base],$right);
+}
